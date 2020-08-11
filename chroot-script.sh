5968c6b3892fdb592da5f664c3df93ffe2fd4ce3
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-08-11 17:26:15 +0100

    Remove fakeroot changes as building on Arm

--- chroot-script.sh
+++ chroot-script.sh
@@ -22,19 +22,6 @@ pacman -S --noconfirm sudo base-devel git go
 useradd -mr aur_builder
 echo "aur_builder ALL=(ALL) NOPASSWD: /usr/bin/pacman" > /etc/sudoers.d/11-aur_builder-pacman
 
-# Install po4a (dependency of fakeroot-tcp)
-pacman -S --noconfirm po4a
-ln -s /usr/bin/vendor_perl/po4a /usr/bin/po4a
-
-# Install fakeroot-tcp to avoid (https://archlinuxarm.org/forum/viewtopic.php?t=14466&p=63662)
-curl https://aur.archlinux.org/cgit/aur.git/snapshot/fakeroot-tcp.tar.gz -o /home/aur_builder/fakeroot-tcp.tar.gz
-sudo -u aur_builder bash -c 'cd ~aur_builder && tar xvf fakeroot-tcp.tar.gz && cd fakeroot-tcp && makepkg -si --noconfirm'
-
-# Install fakeroot-tcp
-curl https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz -o /home/aur_builder/yay.tar.gz
-sudo -u aur_builder bash -c 'cd ~aur_builder && tar xvf yay.tar.gz && cd yay && makepkg -si --noconfirm'
-rm -Rf ~aur_builder/yay
-
 # Install yay
 useradd -mr aur_builder
 echo "aur_builder ALL=(ALL) NOPASSWD: /usr/bin/pacman" > /etc/sudoers.d/11-aur_builder-pacman
