182733aaf422dc7d5cf416f8f1ff123fb35a8e71
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-08-11 17:36:28 +0100

    Fix duplicated aur_builder creation

--- chroot-script.sh
+++ chroot-script.sh
@@ -19,12 +19,12 @@ echo "Kernel installed"
 pacman -S --noconfirm sudo base-devel git go
 
 # Create aur_builder user for building AUR packages
+set +e
 useradd -mr aur_builder
+set -e
 echo "aur_builder ALL=(ALL) NOPASSWD: /usr/bin/pacman" > /etc/sudoers.d/11-aur_builder-pacman
 
 # Install yay
-useradd -mr aur_builder
-echo "aur_builder ALL=(ALL) NOPASSWD: /usr/bin/pacman" > /etc/sudoers.d/11-aur_builder-pacman
 curl https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz -o /home/aur_builder/yay.tar.gz
 sudo -u aur_builder bash -c 'cd ~aur_builder && tar xvf yay.tar.gz && cd yay && makepkg -si'
 rm -Rf ~aur_builder/yay
