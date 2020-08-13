92338ef6e0c8fa6afe74b814e9fc09e35a2609fb
Alan Jenkins <alan.james.jenkins@gmail.com>
2020-08-13 07:46:31 +0100

    Try fix ownership issues

--- chroot-script.sh
+++ chroot-script.sh
@@ -25,6 +25,7 @@ set -e
 echo "aur_builder ALL=(ALL) NOPASSWD: /usr/bin/pacman" > /etc/sudoers.d/11-aur_builder-pacman
 
 # Install yay
+chown -R aur_builder:aur_builder /home/aur_builder
 curl https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz -o /home/aur_builder/yay.tar.gz
 sudo -u aur_builder bash -c 'cd ~aur_builder && tar xvf yay.tar.gz && cd yay && yes | makepkg -si --noconfirm'
 rm -Rf ~aur_builder/yay
