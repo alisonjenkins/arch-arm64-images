a358cd2b8e137bf3dd52d5ed148ba4e84d067378
Alan Jenkins <alan.james.jenkins@gmail.com>
2020-08-13 23:11:24 +0100

    Unmount dev and run as -R doesn't work

--- build.sh
+++ build.sh
@@ -87,7 +87,8 @@ rm /mnt/chroot-script.sh
 ln -sf /run/systemd/resolve/resolv.conf /mnt/etc/resolv.conf
 
 # Unmount loopback devices
-fuser -k /mnt
+umount /mnt/dev
+umount /mnt/run
 umount -R /mnt
 
 # Zero empty space
