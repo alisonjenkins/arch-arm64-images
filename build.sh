aa04fa2f0c2f681b7fc268f3b26674eb67df3164
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-08-21 12:08:50 +0100

    Fix image creation

--- build.sh
+++ build.sh
@@ -2,38 +2,17 @@
 set -euo pipefail
 
 # Partition sizes
-BOOT_SIZE_MB=300
 ROOT_SIZE_MB=3000
-
-function create_image_file() {
-  local IMG_PATH=$1
-  local BOOT_SIZE_MB=$2
-  local ROOT_SIZE_MB=$3
-
-  IMAGE_SIZE=$(( BOOT_SIZE_MB + ROOT_SIZE_MB + 3))
-
-  echo "Creating empty image at $IMG_PATH with the size of $IMAGE_SIZE"
-  test -f "$IMG_PATH" || dd if=/dev/zero of="$IMG_PATH" bs=1M count="$IMAGE_SIZE"
-}
-
-function partition_image_file() {
-  local IMG_PATH=$1
-  local BOOT_SIZE_MB=$2
-  local ROOT_SIZE_MB=$3
-
-  parted -s "$IMG_PATH" "mklabel msdos"
-  parted --align optimal -s "$IMG_PATH" "mkpart primary fat32 2048s $((BOOT_SIZE_MB + 1))M"
-  parted --align optimal -s "$IMG_PATH" "mkpart primary xfs $((BOOT_SIZE_MB + 2))M 100%"
-  parted -s "$IMG_PATH" "toggle 1 boot"
-}
 
 IMG_PATH="$(pwd)/arch-aarch64.img"
 
 # Create a file to partition to create the image
-create_image_file "$IMG_PATH" "$BOOT_SIZE_MB" "$ROOT_SIZE_MB"
+test -f "$IMG_PATH" || dd if=/dev/zero of="$IMG_PATH" bs=1M count="$IMAGE_SIZE"
 
 # Partition the image
-partition_image_file "$IMG_PATH" "$BOOT_SIZE_MB" "$ROOT_SIZE_MB"
+parted -s "$IMG_PATH" "mklabel msdos"
+parted --align optimal -s "$IMG_PATH" "mkpart primary fat32 2048s ${ROOT_SIZE_MB}M"
+parted -s "$IMG_PATH" "toggle 1 boot"
 
 # Setup loopback devices
 LBDEV=$(losetup --find --show --partscan "$IMG_PATH")
@@ -41,13 +20,10 @@ LBDEV=$(losetup --find --show --partscan "$IMG_PATH")
 sleep 1
 
 # Format partitions
-mkfs.vfat -n "BOOT" -F32 "${LBDEV}p1"
-mkfs.ext4 -L "ROOT" "${LBDEV}p2"
+mkfs.ext4 -L "ROOT" "${LBDEV}p1"
 
 # mount partitions
-mount "${LBDEV}p2" /mnt/
-mkdir /mnt/boot
-mount "${LBDEV}p1" /mnt/boot
+mount "${LBDEV}p1" /mnt/
 
 # Download the Arch aarch64 base
 ARCH_BASE_TAR_PATH="$(pwd)/arch-aarch64.tar.gz"
@@ -86,11 +62,11 @@ rm /mnt/chroot-script.sh
 ln -sf /run/systemd/resolve/resolv.conf /mnt/etc/resolv.conf
 
 # Unmount loopback devices
-umount -R /mnt
+umount /mnt
 
 # Zero empty space
 yum install zerofree
-zerofree "${LBDEV}p2"
+zerofree "${LBDEV}p1"
 
 # Delete loopback devices
 losetup -D "$LBDEV"
